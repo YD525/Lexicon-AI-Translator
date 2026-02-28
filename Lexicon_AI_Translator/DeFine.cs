8b116ac359b4e43d324fba50573a1cce9fd5f3b4
YD525 <525084464@qq.com>
2026-03-01 06:09:47 +0800

    IDE highlighting control statements

--- Lexicon_AI_Translator/DeFine.cs
+++ Lexicon_AI_Translator/DeFine.cs
@@ -38,7 +38,7 @@ namespace LexTranslator
 
         public static string BackupPath = @"\BackUpData\";
 
-        public static string CurrentVersion = "3.5.5.5";
+        public static string CurrentVersion = "3.5.5.6";
         public static LocalSetting GlobalLocalSetting = new LocalSetting();
 
         public static MainGui WorkingWin = null;
