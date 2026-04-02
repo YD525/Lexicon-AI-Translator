16e63f1c1c4666f52780fbc5377b84115c1b74b1
YD525 <525084464@qq.com>
2026-04-02 13:08:19 +0800

    UPDate

--- Lexicon_AI_Translator/MainGui.xaml.cs
+++ Lexicon_AI_Translator/MainGui.xaml.cs
@@ -141,21 +141,21 @@ namespace LexTranslator
         public IntPtr MainHwnd = IntPtr.Zero;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            ChartData DataRef = new ChartData();
+            //ChartData DataRef = new ChartData();
 
-            YDChart.SetAction(
-                new Action<RealtimeLineChart>((Ref) =>
-                {
-                    Ref.PushValue(DataRef.SetCurrent(
-                        new Random(Guid.NewGuid().GetHashCode()).Next(1000, 9999)
-                        ));
-                }),
-                new Action<RealtimeLineChart>((Ref) =>
-                {
-                    Ref.PushValue(DataRef.Total);
-                }),
-                DataRef
-                );
+            //YDChart.SetAction(
+            //    new Action<RealtimeLineChart>((Ref) =>
+            //    {
+            //        Ref.PushValue(DataRef.SetCurrent(
+            //            new Random(Guid.NewGuid().GetHashCode()).Next(1000, 9999)
+            //            ));
+            //    }),
+            //    new Action<RealtimeLineChart>((Ref) =>
+            //    {
+            //        Ref.PushValue(DataRef.Total);
+            //    }),
+            //    DataRef
+            //    );
 
             DeFine.Init(this);
 
