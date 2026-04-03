6317e81768281246abb805d6f7890d697a81cd25
YD525 <525084464@qq.com>
2026-04-03 18:15:31 +0800

    UPDate

--- Lexicon_AI_Translator/MainGui.xaml.cs
+++ Lexicon_AI_Translator/MainGui.xaml.cs
@@ -144,6 +144,7 @@ namespace LexTranslator
   
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            new SplashWindow().Show();
             DeFine.Init(this);
 
              YDChart.SetAction(
