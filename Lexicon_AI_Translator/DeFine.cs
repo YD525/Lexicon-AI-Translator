1b26e8c256f0b73f730cb510fa58333e5994da5f
YD525 <525084464@qq.com>
2026-06-25 00:11:50 +0800

    Update

--- Lexicon_AI_Translator/DeFine.cs
+++ Lexicon_AI_Translator/DeFine.cs
@@ -38,7 +38,7 @@ namespace LexTranslator
 
         public static string BackupPath = @"\BackUpData\";
 
-        public static string CurrentVersion = "3.8.2.6";
+        public static string CurrentVersion = "3.8.2.7";
         public static LocalSetting GlobalLocalSetting = new LocalSetting();
 
         public static MainGui WorkingWin = null;
