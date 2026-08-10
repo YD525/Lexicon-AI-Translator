6f5a3d99945dfd2ad11e0a8985ec493de52d1276
YD525 <525084464@qq.com>
2026-08-10 23:25:13 +0800

    Update.

--- Lexicon_AI_Translator/LexGui.xaml.cs
+++ Lexicon_AI_Translator/LexGui.xaml.cs
@@ -43,7 +43,6 @@ namespace LexTranslator
         public TranslateConfig TranslateConfigView = null;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            new EMPTY().Show();
             DeFine.Init(this);
 
             DeFine.GlobalLocalSetting.ReadConfig();
