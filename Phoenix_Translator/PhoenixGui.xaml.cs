fb386ee3292837c73c6c260f55037db18f5ef338
YD525 <525084464@qq.com>
2026-08-24 17:28:54 +0800

    Update.

--- Phoenix_Translator/PhoenixGui.xaml.cs
+++ Phoenix_Translator/PhoenixGui.xaml.cs
@@ -56,8 +56,7 @@ namespace PhoenixTranslator
         {
             try
             {
-                throw new Exception("Test");
-                new ModFileDialog().Show();
+                //new ModFileDialog().Show();
 
                 PhoenixApp.Init(this);
 
